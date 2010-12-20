21683f4c60f576217d7af326c29d3545a6609f17
Hynek Musil <hynek.musil@gmail.com>
2010-12-20 22:39:24 +0100

    War with IE - Fighting for Standards !

--- changeContent.js
+++ changeContent.js
@@ -1,4 +1,12 @@
 //encoding=UTF-8
+
+/*@cc_on
+(function(f){
+ window.setTimeout =f(window.setTimeout);
+ window.setInterval =f(window.setInterval);
+})(function(f){return function(c,t){var a=[].slice.call(arguments,2);return f(function(){c.apply(this,a)},t)}});
+@*/
+
 try{
 var formax = new function() {
 	var threadId = 0;
